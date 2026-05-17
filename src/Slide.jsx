11948fb65944378fa171a4202b0464d3e58429f0
Matt Jennings <matt@mattjennings.net>
2026-05-16 19:16:03 -0700

    middle of getting carousel to work

--- src/Slide.jsx
+++ src/Slide.jsx
@@ -1,8 +1,22 @@
 export default function Slide({ image }) {
   return (
     <figure>
-      <img src={image.src} alt={image.description} width="600" height="400" />
-      <figcaption>{image.description}</figcaption>
+      {image?.src && image?.description ? (
+        <>
+          <img
+            src={image.src}
+            alt={image.description}
+            width="600"
+            height="400"
+          />
+          <figcaption>{image.description}</figcaption>
+        </>
+      ) : (
+        <>
+          <div className="image-not-found-placeholder"></div>
+          <figurecaption>Image not found</figurecaption>
+        </>
+      )}
     </figure>
   );
 }
